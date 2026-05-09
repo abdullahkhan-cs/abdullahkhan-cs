8a402695935494565136e61d21f51fa66358b5a1
Abdullah Khan <abdullah.malokhani@gmail.com>
2026-05-09 14:24:38 +0500

    added

--- script.js
+++ script.js
@@ -44,3 +44,23 @@ tabLinks.forEach(link => {
     }
   });
 });
+
+// Modal Logic
+function openModal(imageSrc) {
+  const modal = document.getElementById('imageModal');
+  const modalImg = document.getElementById('fullImage');
+  modal.style.display = "block";
+  modalImg.src = imageSrc;
+}
+
+function closeModal() {
+  document.getElementById('imageModal').style.display = "none";
+}
+
+// Close modal when clicking outside the image
+window.onclick = function(event) {
+  const modal = document.getElementById('imageModal');
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+}
